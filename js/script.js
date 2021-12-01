71fdf6f734de3534a5dd0541caa399c6511344c3
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-01 17:45:07 +0100

    seleziona difficolta. cambia griglia.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 function createGrid(difficulty) {
     const container = document.querySelector('.container');
+    //rimuovo ogni possibile griglia precedente ... verranno cercati anche altri metodi
+    container.querySelectorAll('*').forEach(i => i.remove());
     let row;
     let col;
     // aggiungi un quadrato col numero
-    if(difficulty == 'easy') {
-        row = 7;
-        col = 7;
-    } else if(difficulty == 'medium') {
-        row = 9;
-        col = 9;
-    } else {
-        row = 10;
-        col = 10;
+    switch(difficulty) {
+        case 'easy': 
+            row = 7;
+            col = 7;
+            break;    
+        case 'medium': 
+            row = 9;
+            col = 9;
+            break;    
+        case 'hard': 
+            row = 10;
+            col = 10;
+            break;    
     }
     for (let i = 0; i < col*row; i++) {
         const square = document.createElement('div');
@@ -21,25 +27,12 @@ function createGrid(difficulty) {
         container.appendChild(square);
     }
 }
-// container.appendChild(squares);
 //acquisisco bottone azione play
 const btn = document.getElementById('btn-play');
 btn.addEventListener('click', function() {
     const select = document.querySelector('select');
-    // console.log(select.value);
+    //creo griglia a seconda delle dimenzioni
     switch(select.value) {
-        case 'easy': 
-            createGrid('easy');
-            console.log('easy');
-            break;
-            case 'medium': 
-            createGrid('medium');
-            console.log('medium');
-            break;
-            createGrid('hard');
-            console.log('hard');
-            case 'hard': 
-            break;
-        // default: console.log('impossibile visualizzare questo messaggio nel nostro caso');
+        default: createGrid(select.value);
     }
 })
