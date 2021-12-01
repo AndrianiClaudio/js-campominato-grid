f312b9b56d763da5e0d77f26d7ea1053a6679588
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-01 17:52:57 +0100

    cella azzurra al click

--- js/script.js
+++ js/script.js
@@ -4,27 +4,31 @@ function createGrid(difficulty) {
     container.querySelectorAll('*').forEach(i => i.remove());
     let row;
     let col;
-    // aggiungi un quadrato col numero
     switch(difficulty) {
         case 'easy': 
             row = 7;
             col = 7;
             break;    
-        case 'medium': 
+            case 'medium': 
             row = 9;
             col = 9;
             break;    
-        case 'hard': 
+            case 'hard': 
             row = 10;
             col = 10;
             break;    
-    }
+        }
     for (let i = 0; i < col*row; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         square.classList.add(`square--${difficulty}`);
+        // aggiungi un quadrato col numero
         square.append(i+1);
         container.appendChild(square);
+        //azioni al click sul quadrato
+        square.addEventListener('click', function() {
+            this.classList.add('click');
+        });
     }
 }
 //acquisisco bottone azione play
