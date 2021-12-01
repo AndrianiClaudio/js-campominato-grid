e219b9d6e8ad8f62231fe74a930ace86a2382752
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-01 17:29:02 +0100

    aggiunta funzione crea griglia.

--- js/script.js
+++ js/script.js
@@ -1,50 +1,44 @@
-
-//post... square_class rimosso con style-inline
-const container = document.querySelector('.container');
-
+function createGrid(difficulty) {
+    const container = document.querySelector('.container');
+    let row;
+    let col;
+    // aggiungi un quadrato col numero
+    if(difficulty == 'easy') {
+        row = 7;
+        col = 7;
+    } else if(difficulty == 'medium') {
+        row = 9;
+        col = 9;
+    } else {
+        row = 10;
+        col = 10;
+    }
+    for (let i = 0; i < col*row; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add(`square--${difficulty}`);
+        square.append(i+1);
+        container.appendChild(square);
+    }
+}
 // container.appendChild(squares);
 //acquisisco bottone azione play
-let col;
 const btn = document.getElementById('btn-play');
 btn.addEventListener('click', function() {
     const select = document.querySelector('select');
     // console.log(select.value);
     switch(select.value) {
         case 'easy': 
-            col = 7;
-            row = 7;
-            // aggiungi un quadrato col numero
-            for (let i = 0; i < col*row; i++) {
-                const square = document.createElement('div');
-                square.classList.add('square-easy');
-                square.append(i+1);
-                container.appendChild(square);
-            }
+            createGrid('easy');
             console.log('easy');
             break;
             case 'medium': 
-            col = 9;
-            row = 9;
-            // aggiungi un quadrato col numero
-            for (let i = 0; i < col * row; i++) {
-                const square = document.createElement('div');
-                square.classList.add('square-medium');
-                square.append(i + 1);
-                container.appendChild(square);
-            }
+            createGrid('medium');
             console.log('medium');
             break;
-            case 'hard': 
-            col = 10;
-            row = 10;
-            // aggiungi un quadrato col numero
-            for (let i = 0; i < col * row; i++) {
-                const square = document.createElement('div');
-                square.classList.add('square-hard');
-                square.append(i + 1);
-                container.appendChild(square);
-            }
+            createGrid('hard');
             console.log('hard');
+            case 'hard': 
             break;
         // default: console.log('impossibile visualizzare questo messaggio nel nostro caso');
     }
