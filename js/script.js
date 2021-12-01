d9aba6a10cd21c430b0df7c3180286016716d8f6
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-01 16:48:20 +0100

    js: inserimento bottone. vedi diffic. selezionata

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('js/script.js caricato correttamente');
+const container = document.querySelector('.container');
+
+//iniziamo con una struttura 7*7.. di default la difficolta é easy
+let col = 7;
+let row = 7;
+let dim = col * row;
+for(let i = 0; i<dim; i++) {
+    // aggiungi un quadrato col numero
+    const square = document.createElement('div');
+    square.classList.add('square-easy');
+    square.append(i+1);
+    container.appendChild(square);
+}
+
+//acquisisco bottone azione play
+const btn = document.getElementById('btn-play');
+btn.addEventListener('click', function() {
+    const select = document.querySelector('select');
+    console.log(select.value);
+})
