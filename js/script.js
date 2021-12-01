9ef76f88cdd3af15300d53286c0af8f9ff0472e6
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-01 17:18:44 +0100

    bottone difficolta (da eliminare vecchia griglia)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,51 @@
+
+//post... square_class rimosso con style-inline
 const container = document.querySelector('.container');
 
-//iniziamo con una struttura 7*7.. di default la difficolta é easy
-let col = 7;
-let row = 7;
-let dim = col * row;
-for(let i = 0; i<dim; i++) {
-    // aggiungi un quadrato col numero
-    const square = document.createElement('div');
-    square.classList.add('square-easy');
-    square.append(i+1);
-    container.appendChild(square);
-}
-
+// container.appendChild(squares);
 //acquisisco bottone azione play
+let col;
 const btn = document.getElementById('btn-play');
 btn.addEventListener('click', function() {
     const select = document.querySelector('select');
-    console.log(select.value);
+    // console.log(select.value);
+    switch(select.value) {
+        case 'easy': 
+            col = 7;
+            row = 7;
+            // aggiungi un quadrato col numero
+            for (let i = 0; i < col*row; i++) {
+                const square = document.createElement('div');
+                square.classList.add('square-easy');
+                square.append(i+1);
+                container.appendChild(square);
+            }
+            console.log('easy');
+            break;
+            case 'medium': 
+            col = 9;
+            row = 9;
+            // aggiungi un quadrato col numero
+            for (let i = 0; i < col * row; i++) {
+                const square = document.createElement('div');
+                square.classList.add('square-medium');
+                square.append(i + 1);
+                container.appendChild(square);
+            }
+            console.log('medium');
+            break;
+            case 'hard': 
+            col = 10;
+            row = 10;
+            // aggiungi un quadrato col numero
+            for (let i = 0; i < col * row; i++) {
+                const square = document.createElement('div');
+                square.classList.add('square-hard');
+                square.append(i + 1);
+                container.appendChild(square);
+            }
+            console.log('hard');
+            break;
+        // default: console.log('impossibile visualizzare questo messaggio nel nostro caso');
+    }
 })
